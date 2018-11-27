2232c0e7f342a49cbcd4e202134f014cb5d85f36
Lucia Reynoso <lvreynoso@protonmail.com>
2018-11-27 15:23:35 -0800

    first two alert actions work

--- collections/CustomFlowLayout.swift
+++ collections/CustomFlowLayout.swift
@@ -14,9 +14,16 @@ class CustomFlowLayout: UICollectionViewFlowLayout {
         super.prepare()
         guard let cv = collectionView else { return }
         
-        self.itemSize = CGSize(width: cv.bounds.inset(by: cv.layoutMargins).size.width, height: 70.0)
+        // self.itemSize = CGSize(width: cv.bounds.inset(by: cv.layoutMargins).size.width, height: 70.0)
         self.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         self.sectionInsetReference = .fromSafeArea
+        
+        let availableWidth = cv.bounds.inset(by: cv.layoutMargins).size.width
+        let minColumnWidth = CGFloat(200)
+        let maxNumColumns = Int(availableWidth/minColumnWidth)
+        let cellWidth = (availableWidth / CGFloat(maxNumColumns)).rounded(.down)
+        
+        self.itemSize = CGSize(width: cellWidth, height: 70.0)
 
     }
 
--- collections/ViewController.swift
+++ collections/ViewController.swift
@@ -13,56 +13,78 @@ class ViewController: UIViewController {
     lazy var collectionView: UICollectionView = {
         
         // Instantiating the UICollectionView, using the default flow layout
-        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        // let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        // Instantiating the UICollectionView, using the custom flow layout
+        let flow = CustomFlowLayout()
+        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flow)
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
         // Setting the datasource & delegate
-   
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         
         // Customization
         collectionView.alwaysBounceVertical = true
         collectionView.backgroundColor = .white
         
         //Registering the cell
+        collectionView.register(Cell.self, forCellWithReuseIdentifier: Cell.identifier)
         
         return collectionView
     }()
     
-    var data: [Int] = Array(0..<10)
+    var data: [Int] = Array(0..<18)
     
     
     override func loadView() {
         super.loadView()
         view.addSubview(collectionView)
     }
-  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Update", style: .done, target: self, action: #selector(showOptions(controller:)))
-
+        
     }
-
+    
     @objc func showOptions(controller: UIViewController) {
         let alert = UIAlertController(title: "Options", message: "Choose an update", preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "Example", style: .default, handler: { (_) in
             self.collectionView.performBatchUpdates({
                 
-                 self.data[3] = 808080
-                 let updated = self.data[3]
-                 self.data.remove(at: 3)
-                 self.data.remove(at: 2)
-                 self.data.insert(updated, at: 0)
-                 
-                 self.collectionView.reloadItems(at: [IndexPath(item: 3, section: 0)])
-                 self.collectionView.deleteItems(at: [IndexPath(item: 2, section: 0)])
-                 self.collectionView.moveItem(at: IndexPath(item: 3, section: 0), to:  IndexPath(item: 0, section: 0))
+                // handling the data source
+                self.data[3] = 808080
+                let updated = self.data[3]
+                self.data.remove(at: 3)
+                self.data.remove(at: 2)
+                self.data.insert(updated, at: 0)
+                
+                // handling the view
+                self.collectionView.reloadItems(at: [IndexPath(item: 3, section: 0)])
+                self.collectionView.deleteItems(at: [IndexPath(item: 2, section: 0)])
+                
                 
             }, completion: nil)
-        
+            self.collectionView.performBatchUpdates({
+                self.collectionView.moveItem(at: IndexPath(item: 3, section: 0), to:  IndexPath(item: 0, section: 0))
+            }, completion: nil)
+            DispatchQueue.main.async {
+                self.collectionView.reloadItems(at: [IndexPath(item: 0, section: 0), IndexPath(item: 1, section: 0), IndexPath(item: 2, section: 0), IndexPath(item: 3, section: 0)])
+            }
+            
         }))
         
         alert.addAction(UIAlertAction(title: "Insert 3 at the beginning", style: .default, handler: { (_) in
+            self.collectionView.performBatchUpdates({
+                self.data.insert(286, at: 0)
+                self.data.insert(386, at: 1)
+                self.data.insert(486, at: 2)
+                
+                self.collectionView.insertItems(at: [IndexPath(item: 0, section: 0), IndexPath(item: 1, section: 0), IndexPath(item: 2, section: 0)])
+                self.collectionView.reloadItems(at: [IndexPath(item: 0, section: 0), IndexPath(item: 1, section: 0), IndexPath(item: 2, section: 0), IndexPath(item: 3, section: 0), IndexPath(item: 4, section: 0), IndexPath(item: 5, section: 0)])
+            }, completion: nil)
         }))
         
         alert.addAction(UIAlertAction(title: "Update value at 5", style: .default, handler: { (_) in
@@ -121,33 +143,34 @@ extension ViewController: UICollectionViewDelegate {
     
 }
 
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 20, height: 20)
-    }
-    
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    
-}
-
+/*
+ extension ViewController: UICollectionViewDelegateFlowLayout {
+ 
+ func collectionView(_ collectionView: UICollectionView,
+ layout collectionViewLayout: UICollectionViewLayout,
+ sizeForItemAt indexPath: IndexPath) -> CGSize {
+ return CGSize(width: collectionView.frame.width / 3.5, height: collectionView.frame.width / 3.5)
+ }
+ 
+ 
+ func collectionView(_ collectionView: UICollectionView,
+ layout collectionViewLayout: UICollectionViewLayout,
+ insetForSectionAt section: Int) -> UIEdgeInsets {
+ return UIEdgeInsets(top: collectionView.frame.width / 32, left: collectionView.frame.width / 32, bottom: collectionView.frame.width / 32, right: collectionView.frame.width / 32)
+ }
+ 
+ func collectionView(_ collectionView: UICollectionView,
+ layout collectionViewLayout: UICollectionViewLayout,
+ minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+ return collectionView.frame.width / 32
+ }
+ 
+ func collectionView(_ collectionView: UICollectionView,
+ layout collectionViewLayout: UICollectionViewLayout,
+ minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+ return collectionView.frame.width / 32
+ }
+ 
+ 
+ }
+ */
